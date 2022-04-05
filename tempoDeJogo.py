51e1c79e3f8dda1ed1c4a83130514d623ac853b0
André L. Porto <andreportol@gmail.com>
2022-04-05 15:28:17 -0400

    problema testado e finalizado.

--- tempoDeJogo.py
+++ tempoDeJogo.py
@@ -5,11 +5,16 @@ class CalculoJogo:
         if self.t_final > self.t_inicial:
             resultado = tempoFinal - tempoInicial
             return resultado
+        elif self.t_inicial>=self.t_final:
+            t1 = 24 - self.t_inicial
+            t2 = self.t_final
+            return t1+t2
+
 
 if __name__=="__main__":
     tempo1 = int(input("Hora inicial: "))
     tempo2 = int(input("Hora final: "))
     jogo1 = CalculoJogo()
-    print(f"O jogo durou {jogo1.calculaTempo(tempo1,tempo2)} hora(s)")
+    print(f"O jogo durou {jogo1.calculaTempo(tempo1,tempo2)} hora(s).")
     
     
