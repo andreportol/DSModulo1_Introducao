364d09577cbe5a8bdbce8a2da45172b3bbf26ee1
André L. Porto <andreportol@gmail.com>
2022-04-05 15:13:07 -0400

    criando a classe CalculoJogo e o metodo calculaTempo.

--- tempoDeJogo.py
+++ tempoDeJogo.py
@@ -1,18 +1,15 @@
-def horaInicial(self, tempoInicial):
-    self.tempoInicial = tempoInicial
-    return self.tempoInicial
-
-def horaFinal(self,tempoFinal):
-    self.tempoFinal = tempoFinal
-    return self.tempoFinal
-
-def calculaTempo(self):
-    if horaFinal() > horaInicial():
-        return horaFinal() - horaInicial()
+class CalculoJogo:
+    def calculaTempo(self, tempoInicial, tempoFinal):
+        self.t_inicial = tempoInicial
+        self.t_final = tempoFinal
+        if self.t_final > self.t_inicial:
+            resultado = tempoFinal - tempoInicial
+            return resultado
 
 if __name__=="__main__":
     tempo1 = int(input("Hora inicial: "))
     tempo2 = int(input("Hora final: "))
-    horaInicial(tempo1)
-    horaFinal(tempo2)
-    print(f" O jogo durou {calculaTempo} hora(s)")
+    jogo1 = CalculoJogo()
+    print(f"O jogo durou {jogo1.calculaTempo(tempo1,tempo2)} hora(s)")
+    
+    
