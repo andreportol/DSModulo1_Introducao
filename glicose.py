61873f38e51615656efe7abfb09d76067d865ff0
André L. Porto <andreportol@gmail.com>
2022-04-12 10:07:00 -0400

    excluindo o return do método calcularGlicose

--- glicose.py
+++ glicose.py
@@ -1,14 +1,13 @@
 class Glicose:
     def calcularGlicose(self, valor_medido):
         if valor_medido <= 100:
-            return print("Classificação: normal")
+            print("Classificação: normal")
         elif valor_medido <= 140:
-            return print("Classificação: elevado")
+            print("Classificação: elevado")
         else:
-            return print("Classificação: diabetes")
+            print("Classificação: diabetes")
 
 if __name__=='__main__':
     valor = float(input("Digite a medida de glicose:"))
     g1 = Glicose()
     g1.calcularGlicose(valor)
-    
