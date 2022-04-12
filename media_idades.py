c2a6f57cfe453e9226cd0f0f222a425de1372f19
André L. Porto <andreportol@gmail.com>
2022-04-12 15:37:45 -0400

    Programa media_idades foi testado e finalizado.

---
+++ media_idades.py
@@ -0,0 +1,22 @@
+from email.errors import InvalidDateDefect
+
+
+class MediaIdades():
+    def calcularMediaIdades():
+        contador = 0
+        soma_idade = 0
+        
+        print("Digite as idades:")
+        idade = int(input())
+        if idade < 0:
+            print("IMPOSSIVEL CALCULAR")
+        else:    
+            while idade >= 0:
+                contador += 1
+                soma_idade = soma_idade + idade
+                idade = int(input())
+            print(f"MEDIA = {soma_idade/contador:.2f}")
+
+
+if __name__=='__main__':
+    MediaIdades.calcularMediaIdades()
