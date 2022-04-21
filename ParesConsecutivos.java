80ef37fb8f811d6cb229aa92d0a400ef1bc1993d
André L. Porto <andreportol@gmail.com>
2022-04-21 10:41:17 -0400

    Otimização do código.

--- ParesConsecutivos.java
+++ ParesConsecutivos.java
@@ -2,36 +2,19 @@ import java.util.Scanner;
 
 public class ParesConsecutivos {
     public static void main(String[] args) {
-        boolean loop = true;
-        int valor_soma = 0;
-        int[] soma = new int[5];
+        int x = 0, soma;
         Scanner s = new Scanner(System.in);
-        while (loop) {
-            valor_soma = 0;
-            System.out.println("Digite um numero inteiro: ");
-            int valor_digitado = Integer.parseInt(s.next());
-            if (valor_digitado == 0) {
-                System.out.println("Exit");
-                break;
-            } else {
-                if (valor_digitado % 2 == 0) {
-                    for (int x = 0; x < 5; x++) {
-                        soma[x] = valor_digitado;
-                        valor_digitado += 2;
-                        valor_soma += soma[x];
-                    }
-                    System.out.println("Soma: " + valor_soma);
-                } else {
-                    valor_digitado += 1;
-                    for (int x = 0; x < 5; x++) {
-                        soma[x] = valor_digitado;
-                        valor_digitado += 2;
-                        valor_soma += soma[x];
-                    }
-                    System.out.println("Soma: " + valor_soma);
-                }
+        do {
+            System.out.print("Digite um numero inteiro: ");
+            x = Integer.parseInt(s.next());
+            if (x % 2 != 0 && x != 0) {
+                x += 1;
+            } 
+            if (x != 0) {
+                soma = 5 * x + 20;
+                System.out.println("Soma: " + soma);
             }
-        }
+        } while (x != 0);
         s.close();
     }
 }
