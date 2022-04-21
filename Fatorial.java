4c698669603b5b7da4fc66d6cd52326a651c7bce
André L. Porto <andreportol@gmail.com>
2022-04-21 17:34:36 -0400

    Programa testado e finalizado.

---
+++ Fatorial.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Fatorial {
+
+    public static void main(String[] args) {
+        
+        Scanner s = new Scanner(System.in);
+        System.out.println("Digite o valor de N:");
+        int n = Integer.parseInt(s.next());
+        int fatorial = 1;
+        for (int i = 1; i <= n; i++) {
+            fatorial = fatorial * i;           
+        }
+        System.out.println("Fatorial: "+fatorial);
+        s.close();
+    }
+}
