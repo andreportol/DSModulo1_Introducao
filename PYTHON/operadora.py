cad66cfb991cabb6152e35d7729b0d1313a7617e
André L. Porto <andreportol@gmail.com>
2022-04-25 17:06:29 -0400

    Add files via upload
    Projeto operadora testado e finalizado.

---
+++ PYTHON/operadora.py
@@ -0,0 +1,14 @@
+class Operadora:
+
+    def __init__(self, minutos):
+        self.franquia = 100
+        self.valorFranquia = 50
+        self.valorMinuto = 2
+        if minutos <= self.franquia:
+            return print(f'Valor a pagar R$ {self.valorFranquia}')
+        else:
+            return print(f'Valor a pagar R$ {(minutos - self.franquia)*self.valorMinuto}')
+
+if __name__ == '__main__':
+    minutos = int(input('Digite a quantidade de minutos: '))
+    Vivo = Operadora(minutos)
