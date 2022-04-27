2e07f9d686fefd8df24cb349c95e9ac08904e4b9
André L. Porto <andreportol@gmail.com>
2022-04-27 13:49:34 -0400

    Programa testado e finalizado.

---
+++ JAVA/TrocoVerificado.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class TrocoVerificado {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Preco unitario do produto: ");
+        double preco = Double.parseDouble(s.next());
+        System.out.println("Quantidade comprada: ");
+        int quantidade = Integer.parseInt(s.next());
+        System.out.println("Dinheiro recebido: ");
+        double dinheiro_recebido = Double.parseDouble(s.next());
+        if (preco * quantidade < dinheiro_recebido) {
+            System.out.println("Troco: " + (dinheiro_recebido - (preco * quantidade)));
+        } else
+            System.out.println("Dinheiro insuficiente. Faltam " + (preco * quantidade - dinheiro_recebido) + " reais.");
+
+        s.close();
+    }
+
+}
