90db0a91b7c74a7c4d9dc5fd26712d42b9d6c446
andreportol <andreportol@gmail.com>
2022-05-12 09:22:20 -0400

    acrescentado mais códigos de exemplos

--- PYTHON/ternario.py
+++ PYTHON/ternario.py
@@ -2,3 +2,11 @@ tempo = False
 print('Hoje esta ' + ('calor.' if tempo else 'frio.'))
 # outra modo
 print('Hoje esta ' + ('frio.', 'calor.')[tempo])
+
+
+preco = 34.5
+desconto = ((preco * 0.1) if preco < 20 else preco * 0.05)
+print(f'desconto: {desconto}')
+            # False       # True
+desconto = (preco * 0.05, preco * 0.1)[preco < 20]
+print(f'desconto: {desconto}')
